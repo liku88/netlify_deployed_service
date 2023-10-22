5310c0c0198fcec737158961194d0427fdbb8fbf
Mangaldeep Das <rockonliku88@gmail.com>
2023-10-22 21:35:22 +0530

    Add files via upload

---
+++ netlify/functions/server.mjs
@@ -0,0 +1,24 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+let lastResponse = null;
+
+app.post('/.netlify/functions/server/subscribe', async (req, res) => {
+  const responseValue = req.body;
+  
+  if (JSON.stringify(responseValue) !== lastResponse) {
+    lastResponse = JSON.stringify(responseValue);
+    const netlifyWebhookUrl = 'https://api.netlify.com/build_hooks/65352a2134f6566c1306db97';
+    await axios.post(netlifyWebhookUrl);
+    res.status(200).send({ message: "New response received, Netlify build triggered!" });
+  } else {
+    res.status(200).send({ message: "Same response received, no action taken." });
+  }
+});
+
+module.exports.handler = serverless(app);
